d114a9b9d90df324dbecaaf6dd815fb41fad22a7
DLF2P <113426828+dlf2pcuso4@users.noreply.github.com>
2022-10-11 10:48:54 +1100

    Update cool-console.js

--- cool-console.js
+++ cool-console.js
@@ -1,4 +1,15 @@
+const font = document.createElement("style");
+font.innerHTML =
+  "@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');";
+document.head.appendChild(font);
+const _popup = document.createElement("div");
+_popup.style.cssText =
+  "position: fixed; overflow: scroll; width: 100%; height: 100%; inset: 0px; padding:30px; background-color: #000000C0; z-index: 2; font-family: Source Code Pro; color: #00FF00; font-size: 20px; font-weight: normal; text-align: left; text-decoration: none;";
+_popup.style.display = "none";
+document.body.appendChild(_popup);
 const _consolePrint = (txt) => {
-  console.log(txt);
+  _popup.innerHTML = _popup.innerHTML.slice(0, -5) + "</p><p>" + txt + "_</p>";
+  _popup.scrollTop = _popup.scrollHeight;
 };
-alert("yo");
+const _consoleShow = () => (_popup.style.display = "block");
+const _consoleHide = () => (_popup.style.display = "none");
