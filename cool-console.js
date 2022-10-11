eceb7305676a246cc8508283c8845b6550124e90
DLF2P <113426828+dlf2pcuso4@users.noreply.github.com>
2022-10-11 13:53:20 +1100

    Update cool-console.js

--- cool-console.js
+++ cool-console.js
@@ -3,7 +3,7 @@ document.head.innerHTML =
   "<style>@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');</style>";
 const popup_hzcIFp0C53 = document.createElement("div");
 popup_hzcIFp0C53.style.cssText =
-  "position: fixed; overflow: scroll; width: 100%; height: 100%; inset: 0px; padding:30px; background-color: #000000C0; z-index: 2; font-family: Source Code Pro; color: #00FF00; font-size: 20px; font-weight: normal; text-align: left; text-decoration: none;";
+  "position: fixed; overflow: scroll; width: 100%; height: 100%; inset: 0px; padding:30px; background-color: #000000C0; z-index: 10000 !important; font-family: Source Code Pro; color: #00FF00; font-size: 20px; font-weight: normal; text-align: left; text-decoration: none;";
 popup_hzcIFp0C53.style.display = "none";
 document.body.appendChild(popup_hzcIFp0C53);
 const consolePrint_MgzkaLC2RU = (txt) => {
