2d940971dd932c25e25a616da5c81536d7558cc1
DLF2P <113426828+dlf2pcuso4@users.noreply.github.com>
2022-10-11 13:04:37 +1100

    Update cool-console.js

--- cool-console.js
+++ cool-console.js
@@ -1,15 +1,16 @@
 document.head.innerHTML =
   document.head.innerHTML +
   "<style>@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');</style>";
-const _popup = document.createElement("div");
-_popup.style.cssText =
+const popup_hzcIFp0C53 = document.createElement("div");
+popup_hzcIFp0C53.style.cssText =
   "position: fixed; overflow: scroll; width: 100%; height: 100%; inset: 0px; padding:30px; background-color: #000000C0; z-index: 2; font-family: Source Code Pro; color: #00FF00; font-size: 20px; font-weight: normal; text-align: left; text-decoration: none;";
-_popup.style.display = "none";
-document.body.appendChild(_popup);
-const _consolePrint = (txt) => {
-  _popup.innerHTML = _popup.innerHTML.slice(0, -5) + "</p><p>" + txt + "_</p>";
-  _popup.scrollTop = _popup.scrollHeight;
+popup_hzcIFp0C53.style.display = "none";
+document.body.appendChild(popup_hzcIFp0C53);
+const consolePrint_MgzkaLC2RU = (txt) => {
+  popup_hzcIFp0C53.innerHTML =
+    popup_hzcIFp0C53.innerHTML.slice(0, -5) + "</p><p>" + txt + "_</p>";
+  popup_hzcIFp0C53.scrollTop = popup_hzcIFp0C53.scrollHeight;
   console.log(txt);
 };
-const _consoleShow = () => (_popup.style.display = "block");
-const _consoleHide = () => (_popup.style.display = "none");
+const consoleShow_LMYz9GUf9m = () => (popup_hzcIFp0C53.style.display = "block");
+const consoleHide_nQQryu3f0m = () => (popup_hzcIFp0C53.style.display = "none");
